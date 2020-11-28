236d04d6b7fafec93b46c59e815d28be39d95fe9
Daniellau331 <daniellau331@gmail.com>
2020-11-28 09:18:18 -0800

    Created static and templates directories

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 @app.get("/")
 async def hello_world():
     return {"Hello": "World"}
 
 
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
