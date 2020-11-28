cdb6487ee9076a50864d6f1824a8f484099e50e2
Daniellau331 <daniellau331@gmail.com>
2020-11-28 10:06:02 -0800

    Updated index.html and imported some css and js files

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
-@app.get("/")
-async def hello_world():
-    return {"Hello": "World"}
+# @app.get("/")
+# async def hello_world():
+#     return {"Hello": "World"}
 
+@app.get("/")
+async def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
 
 if __name__ == '__main__':
     import uvicorn
